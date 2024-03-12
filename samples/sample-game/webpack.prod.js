04fe9ffb10fa2549d47618469ffc9415f37110f4
Shane McGowan <shane.mcgowan@intercom.io>
2024-03-12 02:27:17 +0000

    [sample-game] webpack usingfunctions

--- samples/sample-game/webpack.prod.js
+++ samples/sample-game/webpack.prod.js
@@ -2,40 +2,42 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      }
-    ],
-  },
-  resolve: {
-    alias: {
-      '@core': path.resolve(__dirname, '../../core/'),
-      '@sample-game': path.resolve(__dirname, './'),
-    },
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: '../../core/client/index.html',
-      filename: './index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: '../../core/client/index.css', to: 'assets/index.css', },
-        { from: './assets', to: 'assets', }
+module.exports = () => {
+  return {
+    entry: './index.ts',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        }
       ],
-    })
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../../dist/sample-game/'),
-  },
-  mode: 'production',
+    },
+    resolve: {
+      alias: {
+        '@core': path.resolve(__dirname, '../../core/'),
+        '@sample-game': path.resolve(__dirname, './'),
+      },
+      extensions: ['.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: '../../core/client/index.html',
+        filename: './index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: '../../core/client/index.css', to: 'assets/index.css', },
+          { from: './assets', to: 'assets', }
+        ],
+      })
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, '../../dist/sample-game/'),
+    },
+    mode: 'production',
+  };
 };
