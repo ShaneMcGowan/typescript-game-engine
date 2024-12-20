7ecaea6a224553f29f72d9107de9b294be9064d8
Shane McGowan <git@shanemcgowan.com>
2024-12-20 00:03:41 +0000

    [game] migrating map - world to use new background system (it is beautiful)

--- game/scenes/game/maps/world/map.ts
+++ game/scenes/game/maps/world/map.ts
@@ -25,18 +25,21 @@ import { TilesetHouse } from '@game/constants/tileset-house.constants';
 import { LockedDoorObject } from '@game/objects/world/locked-door.object';
 import { HoleObject } from '@game/objects/world/hole.object';
 import { SCENE_GAME_MAP_SHOP } from '../shop/map';
+import { JsonBackgroundMap } from '@core/model/background';
+import background from './background.json';
 
 export class SCENE_GAME_MAP_WORLD extends SceneMap {
   // height = 100;
   // width = 100;
 
-  backgroundLayers: BackgroundLayer[] = [
-    SCENE_GAME_MAP_WORLD_BACKGROUND_WATER,
-    SCENE_GAME_MAP_WORLD_BACKGROUND_GROUND,
-    SCENE_GAME_MAP_WORLD_BACKGROUND_MOUNTAINS,
-    SCENE_GAME_MAP_WORLD_BACKGROUND_STEPS,
-    // SCENE_GAME_MAP_WORLD_BACKGROUND_BRIDGES,
-  ];
+  // backgroundLayers: BackgroundLayer[] = [
+  //   SCENE_GAME_MAP_WORLD_BACKGROUND_WATER,
+  //   SCENE_GAME_MAP_WORLD_BACKGROUND_GROUND,
+  //   SCENE_GAME_MAP_WORLD_BACKGROUND_MOUNTAINS,
+  //   SCENE_GAME_MAP_WORLD_BACKGROUND_STEPS,
+  //   // SCENE_GAME_MAP_WORLD_BACKGROUND_BRIDGES,
+  // ];
+  background: JsonBackgroundMap = background;
 
   constructor(protected scene: SCENE_GAME) {
     super(scene);
@@ -156,138 +159,18 @@ export class SCENE_GAME_MAP_WORLD extends SceneMap {
       }
     }
 
-    // fences
-    for (let row = 0; row < 19; row++) {
-      for (let col = 0; col < 1; col++) {
-        let type = row === 18 ? FenceType.FencePost : FenceType.MiddleVertical
-
-        this.scene.addObject(
-          new FenceObject(scene, { positionX: 27, positionY: 0 + row, type: type })
-        );
-      }
-    }
-
-    for (let row = 0; row < 1; row++) {
-      for (let col = 0; col < 28; col++) {
-        let type = col === 27 ? FenceType.BottomRight : FenceType.MiddleHorizontal
-
-        this.scene.addObject(
-          new FenceObject(scene, { positionX: 0 + col, positionY: 18, type: type })
-        );
-      }
-    }
-
-
-    // fences
-
-    // 0
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 25, positionY: 0, type: FenceType.MiddleVertical }),
-      new FenceObject(scene, { positionX: 26, positionY: 0, type: FenceType.FencePost }),
-    ]);
-    // 1
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 25, positionY: 2, type: FenceType.MiddleVertical })
-    ]);
-    // 2
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 25, positionY: 1, type: FenceType.MiddleVertical })
-    ]);
-    // 3
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 19, positionY: 3, type: FenceType.TopLeft }),
-      new FenceObject(scene, { positionX: 20, positionY: 3, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 21, positionY: 3, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 22, positionY: 3, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 23, positionY: 3, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 24, positionY: 3, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 25, positionY: 3, type: FenceType.BottomRight }),
-    ]);
-    // 4
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 19, positionY: 4, type: FenceType.MiddleVertical })
-    ]);
-    // 5
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 19, positionY: 5, type: FenceType.MiddleVertical })
-    ]);
-    // 6
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 19, positionY: 6, type: FenceType.MiddleVertical })
-    ]);
-    // 7
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 7, type: FenceType.TopLeft }),
-      new FenceObject(scene, { positionX: 5, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 6, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 7, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 8, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 9, positionY: 7, type: FenceType.RightHorizontal }),
-      //
-      new FenceObject(scene, { positionX: 11, positionY: 7, type: FenceType.LeftHorizontal }),
-      new FenceObject(scene, { positionX: 12, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 13, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 14, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 15, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 16, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 17, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 18, positionY: 7, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 19, positionY: 7, type: FenceType.BottomRight }),
-    ]);
-    // 8
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 8, type: FenceType.MiddleVertical })
-    ]);
-    // 9
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 9, type: FenceType.MiddleVertical })
-    ]);
-    // 10
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 10, type: FenceType.MiddleVertical })
-    ]);
-    // 11
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 11, type: FenceType.MiddleVertical })
-    ]);
-    // 12
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 12, type: FenceType.MiddleVertical })
-    ]);
-    // 13
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 4, positionY: 13, type: FenceType.MiddleVertical })
-    ]);
-    // 14
-    this.scene.addObjects([
-      new FenceObject(scene, { positionX: 0, positionY: 14, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 1, positionY: 14, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 2, positionY: 14, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 3, positionY: 14, type: FenceType.MiddleHorizontal }),
-      new FenceObject(scene, { positionX: 4, positionY: 14, type: FenceType.BottomRight })
-    ]);
-    // 15
-    this.scene.addObjects([]);
-    // 16
-    this.scene.addObject(new FenceObject(scene, { positionX: 0, positionY: 16, type: FenceType.FencePost }));
-    // 17
-    this.scene.addObject(new FenceObject(scene, { positionX: 0, positionY: 17, type: FenceType.FencePost }));
-    // 18
-    this.scene.addObject(new FenceObject(scene, { positionX: 0, positionY: 18, type: FenceType.FencePost }));
-    // 19
-    this.scene.addObjects([]);
-    // 20
-    this.scene.addObjects([]);
-
-
-    this.scene.addObject(new CollisionObject(scene, { positionX: 25, positionY: 1.5, height: 4 }));
-    this.scene.addObject(new CollisionObject(scene, { positionX: 19, positionY: 5.5, height: 4 }));
-
-    this.scene.addObject(new CollisionObject(scene, { positionX: 22.5, positionY: 4, width: 6 }));
-    this.scene.addObject(new CollisionObject(scene, { positionX: 6.5, positionY: 8, width: 6 }));
-    this.scene.addObject(new CollisionObject(scene, { positionX: 15, positionY: 8, width: 9 }));
-    this.scene.addObject(new CollisionObject(scene, { positionX: 4, positionY: 11.5, height: 6 }));
-    this.scene.addObject(new CollisionObject(scene, { positionX: 2.25, positionY: 15, width: 4.5 }));
+    // horizontal
+    this.scene.addObject(new CollisionObject(scene, { positionX: 25, positionY: 0, width: 3 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 19, positionY: 3, width: 7 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 4, positionY: 7, width: 6 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 11, positionY: 7, width: 9 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 0, positionY: 14, width: 5 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 0, positionY: 18, width: 28 }));
+    // vertical
+    this.scene.addObject(new CollisionObject(scene, { positionX: 4, positionY: 8, height: 6 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 19, positionY: 4, height: 3 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 25, positionY: 1, height: 2 }));
+    this.scene.addObject(new CollisionObject(scene, { positionX: 27, positionY: 1, height: 17 }));
 
     this.scene.addObject(new ObjectTrackingCameraObject(scene, { object: player }));
 
